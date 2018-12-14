76cb736eaca54e517d6debbb20adc383e62fe314
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-13 18:04:21 -0800

    Create and styled homepage

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/sign_up_container';
+import HomePage from './homepage/home_page';
 import { Link,Switch, Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute} from '../util/auth_route';
 
@@ -12,15 +13,14 @@ const App = () => (
       <GreetingContainer />
     </header>
     <Switch>
-      <div className="main-container">
+      
         <div className="credentials-container">
           <AuthRoute exact path="/login" component={LoginFormContainer} />
           <AuthRoute exact path="/signup" component={SignupFormContainer} />
+          <Route exact path="/" component={HomePage} />
         </div>
-        {/* <div className="background-pic">
-
-        </div> */}
-      </div>
+        
+      
       
     </Switch>
     
---
+++ frontend/components/homepage/home_page.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const HomePage = () => {
+    return(
+        <div class="class-main">
+            <div className="background-pic">
+
+            </div>
+            <div class="description">
+                <div class="detail">
+                    <h1 class="detail-title">Show up to play</h1>
+                    <p class="mes">You and a few others join a host at a court.</p>
+                </div>
+                <div class="detail">
+                    <h1 class="detail-title">Talk and play</h1>
+                    <p class="mes">You will talk and have fun playing.</p>
+                </div>
+                <div class="detail">
+                    <h1 class="detail-title">See who wins</h1>
+                    <p class="mes">That's it. No strings attached.</p>
+                </div>
+            
+            </div>
+        
+        </div>
+    )
+}
+
+export default HomePage;
