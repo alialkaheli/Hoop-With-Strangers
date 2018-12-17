a7b3ec3a54d12231f21aa1a74eeeff23bbb02ecf
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-16 21:54:37 -0800

    Modified to add routes for the event's components

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,7 +6,11 @@ import HomePage from './homepage/home_page';
 import { Link, Switch, Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute} from '../util/auth_route';
 
-import AllCities from './city/all_city_container';
+import EventIndexContainer from './event/index_event_container';
+import EventShowContainer from './event/show_event_container';
+import CreateEvent from './event/event_create';
+import EditEvent from './event/event_edit';
+// import AllCities from './city/all_city_container';
 
 const App = () => (
   <div className='home-content'>
@@ -19,6 +23,12 @@ const App = () => (
           <AuthRoute exact path="/login" component={LoginFormContainer} />
           <AuthRoute exact path="/signup" component={SignupFormContainer} />
           {/* <ProtectedRoute exact path='/cities' component={AllCities} /> */}
+          
+
+          <Route exact path="/events" component={EventIndexContainer}/>
+          <ProtectedRoute exact path="/events/new" component={CreateEvent} />
+          <ProtectedRoute exact path="/events/:eventId" component={EventShowContainer}/>
+          <ProtectedRoute  exact path="/events/:eventId/edit" component={EditEvent} />
           <Route exact path="/" component={HomePage} />
       </Switch>
       </div>
