be370cff06a35a97525946e4012e550b0a30ce41
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-18 11:52:21 -0800

    created join table to join user and event

--- app/controllers/api/join_tables_controller.rb
+++ app/controllers/api/join_tables_controller.rb
@@ -1,2 +1,21 @@
 class Api::JoinTablesController < ApplicationController
+    def create
+        @join = JoinTable.new(join_table_params)
+        if @join.save
+          render :show
+        else
+          render json: @join.errors.full_messages, status: 422
+        end
+      end
+    
+      def destroy
+        @join = current_user.joins.find(params[:id])
+        @join.destroy
+        render :show
+      end
+    
+      def join_table_params
+        params.require(:join_table).permit(:user_id, :event_id)
+      end
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_042051) do
+ActiveRecord::Schema.define(version: 2018_12_18_180815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2018_12_17_042051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "city", null: false
+    t.string "url"
+    t.integer "players"
   end
 
   create_table "join_tables", force: :cascade do |t|
