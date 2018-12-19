6be7e60f411edadda14e3e85059936579f6a2cf9
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-18 17:15:57 -0800

    Joined table reducer created

--- frontend/reducers/join_reducer.js
+++ frontend/reducers/join_reducer.js
@@ -5,9 +5,14 @@ const joinReducer = (state={}, action)=> {
     Object.freeze(state);
     switch(action.type){
         case CREATEJOIN:
-            
+        return merge({}, state, {
+            [action.payload.join.user_id]: action.payload.join
+          });
         case DELETEJOIN:
-
+            let newState = merge({}, state);
+            newState[action.event.id] = action.event;
+            delete newState[action.event.id];
+            return newState;
         default:
         return state;
     }
