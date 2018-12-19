e581cdd55449039bb7f3ef937e3eb8767293199a
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-18 17:14:24 -0800

    create joined table actions

--- frontend/actions/join_actions.js
+++ frontend/actions/join_actions.js
@@ -2,24 +2,25 @@ import * as joinUtil from '../util/join_util';
 export const CREATEJOIN = "CREATEJOIN";
 export const DELETEJOIN = "CREATEJOIN";
 
-const receiveJoin = join => ({
-    join, 
+const receiveJoin = payload => ({
+    payload, 
     type: CREATEJOIN
-}),
+})
 
-const removeJoin = join => ({
-    joinId: join.id,
+const removeJoin = payload => ({
+    join: payload.join,
+    event: payload.event,
     type: DELETEJOIN
 })
 
-export const fetchCreate = join => dispatch => {
-    joinUtil.joinCreate(join).then(join => 
-        dispatch(receiveJoin(join)))
+export const createJoin = join => dispatch => {
+    joinUtil.joinCreate(join).then(payload => 
+        dispatch(receiveJoin(payload)))
 }
 
-export const fetchCreate = join => dispatch => {
-    joinUtil.joinCreate(join.id).then(join => 
-        dispatch(removeJoin(join)))
+export const deleteJoin= join => dispatch => {
+    joinUtil.joinDelete(join.id).then(payload => 
+        dispatch(removeJoin(payload)))
 }
 
 
