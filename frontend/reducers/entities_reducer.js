e0581d4af700c538180a583c540898e4df3cfc08
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-18 17:15:17 -0800

    Joined table reducer also added it in the entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import usersReducer from './users_reducer';
 import eventsReducer from './event_reducer';
+import joinReducer from './join_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   events: eventsReducer,
+  join: joinReducer,
 });
 
 export default entitiesReducer;
