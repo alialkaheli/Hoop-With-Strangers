f5b83249b965386067487f1387eef0671fa83d21
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-21 12:06:16 -0800

    Add a footer part of the homepage/splashpage

--- frontend/components/homepage/home_page.jsx
+++ frontend/components/homepage/home_page.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
                     <div className="hook">
                         <h1 className="m1">Don't play alone</h1>
                         <p className="m2"> But you realize that when you are at a park alone.</p>
-                        <Link to="/signup" className="b1">Join a Squat</Link>
+                        <Link to="/events" className="b1">Checkout Events</Link>
                     </div>
                 </div>
             </div>
@@ -26,7 +26,19 @@ const HomePage = () => {
                     <h1 className="detail-title">See who wins</h1>
                     <p className="mes">That's it. No strings attached.</p>
                 </div>
+
             
+            </div>
+            <div className="home-footer">
+                <div className='list'>
+                    <a href="https://www.facebook.com/ali.alkaheli96">Facebook</a>
+                    <a href="https://www.linkedin.com/in/ali-alkaheli-089817124/">LinkedIn</a>
+                </div>
+                <div className="aboutus">
+                <p>Hoop With Strangers is all about making our cities feel more like neighborhoods. We're more "connected" than ever before, but we're also more alone. And all we want to do is bring people together because, well, the world is better that way.</p>
+
+                <p>We're not doing anything groundbreaking. We're creating something that would've been incredibly unnecessary 20 years ago. But while we get busier, it's easy to forget the value of having fun. A game that's intentionally unintentional.</p>
+                </div>
             </div>
         
         </div>
