29b2303ded2352954a4792dd46c08c8fcdb23f1f
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-21 10:58:51 -0800

    modified for styling

--- frontend/components/dashboard/main_dash.jsx
+++ frontend/components/dashboard/main_dash.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import HostDelete from './host_delete';
+import UserLeave from './leave_event';
 
 class Dashboard extends React.Component {
     constructor(props){
@@ -14,6 +15,7 @@ class Dashboard extends React.Component {
         if (!this.props.joinedEvents && !this.props.hostedEvents) {
             return <div> NO EVENTS!</div>;
         }else{
+            
             return(
                 
                 <div className="dash-content">
@@ -22,15 +24,24 @@ class Dashboard extends React.Component {
                         <p className="message">What are you grateful for today?</p>
                         <div className="join-link"><Link to={'/events'}>Sign up to join a match!</Link></div>
                     </div>
+
+
                     <div className="user-events">
-                        {/* <div> JOINED
+                        <label>JOINED
+                        <div className="hosted-events">
                             {this.props.joinedEvents.map(event => {
-                            return (<EventIndexItem key={event.id} event={event} deleteEvent={this.props.deleteJoin}/>)})}
-                        </div> */}
-                        <div> HOSTED
+                                
+                            return (<UserLeave key={event.id} event={event} deleteJoin={this.props.deleteJoin}/>)})}
+                        </div> 
+                        </label>
+
+
+                        <label> Hosted
+                        <div className="hosted-events">
                         {this.props.hostedEvents.map(event => {
                             return (<HostDelete key={event.id} event={event} deleteEvent={this.props.deleteEvent}/>)})}    
                         </div>
+                        </label>
                     </div>
                     
                 </div>
