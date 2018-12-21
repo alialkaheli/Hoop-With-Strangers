e0046564c505b8c60e4fe88754b8572dfbd744b5
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-21 11:02:43 -0800

    Modified for styling

--- frontend/components/event/show_event.jsx
+++ frontend/components/event/show_event.jsx
@@ -12,22 +12,71 @@ class EventShow extends React.Component {
     if (!event) {
       return (<div>There none</div>);
     }
+
+    // debugger
+    let joinButton = (<button
+      onClick={() =>
+        this.props.createJoin({
+          event_id: this.props.match.params.eventId,
+          user_id: this.props.currentUserId
+        })}
+      
+      
+    >
+      SIGN ME UP!
+  </button>)
+
+  let leaveButton = (<button
+    onClick={() => this.props.deleteJoin(this.props.event.id)}
+    
+    >
+    Leave This Game
+  </button>)
+  let name = ""
+  
+
+  let actionButton = this.props.currentUserJoin ? leaveButton : joinButton;
+    // debugger
     return(
       <div className="show-main">
+        
         <div className="show-content"> 
-          <div className="event-box">
-            <h1 className="toPlay">JOIN TO PLAY</h1>
-            <p className="date">⏰{event.time}<br/></p>
-            <p className="date">📅{event.date}<br /> </p>
-            <p className="info">🗺{event.city}<br /> </p>
-            <p className="info">📍{event.address}</p>
-            <p className="info">🗣{event.url}</p>
-            
+          <div className="sidebar">
+            <div className="event-box">
+              <h1 className="toPlay">JOIN TO PLAY</h1>
+              <p className="date">⏰{event.time}<br/></p>
+              <p className="date">📅{event.date}<br /> </p>
+              <p className="info">🗺{event.city}<br /> </p>
+              <p className="info">📍{event.address}</p>
+              <p className="info">🗣{event.url}</p>   
+            </div>
+            <div className="side-button">
+              {actionButton}
+            </div>
+            <div className="hoop-description">
+              <h1>WHAT IS BASKETBALL TIME, EXACTLY? </h1>
+
+              <h2>🍵</h2>
+              <p>Basketball time is where ten-ish strangers meet at a basketball court with a Host to have a fun basketball game. It's not about anything in particular.</p>
+
+              <p>You're meant to know pretty much nothing about the people that come, aside from the fact that they all signed up for this — just like you. It's self-selecting in that sense.</p>
+
+              <p>However, it may bring you comfort to know a little something about your Host and have fun with them. If so, just keep reading</p>
+
+                <h3>👉</h3>
+
+                
+            </div>
           </div>
           <div className="show-des">
-            <h1 className="meet-host"> Meet Your Host </h1>
+            <h1 className="meet-host"> Get To Meet Your Host </h1>
+              <div className="profile-pic"></div>
             <p className="story">Description</p>
             <p className="host-description">{event.description}</p>
+
+            
+
+
           </div>
         </div>
       </div>
