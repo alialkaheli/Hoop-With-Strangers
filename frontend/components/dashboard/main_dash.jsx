5fdb0faae8115e2f9c257f76b701154d68f377c7
ali alkaheli <ali.alkaheli96@gmail.com>
2019-01-24 09:47:22 -0800

    Modified dashboard page

--- frontend/components/dashboard/main_dash.jsx
+++ frontend/components/dashboard/main_dash.jsx
@@ -16,36 +16,35 @@ class Dashboard extends React.Component {
             return <div> NO EVENTS!</div>;
         }else{
             
-            return(
-                
-                <div className="dash-content">
-                    <div className="dash-side">
-                        <h1 className="welcome">Welcome home!</h1>
-                        <p className="message">What are you grateful for today?</p>
-                        <div className="join-link"><Link to={'/events'}>Sign up to join a match!</Link></div>
-                    </div>
-
-
-                    <div className="user-events">
-                        <label>Events You JOINED...
-                        <div className="hosted-events">
-                            {this.props.joinedEvents.map(event => {
-                                
-                            return (<UserLeave key={event.id} event={event} deleteJoin={this.props.deleteJoin}/>)})}
-                        </div> 
-                        </label>
-
-
-                        <label> Events You HOSTED...
-                        <div className="hosted-events">
-                        {this.props.hostedEvents.map(event => {
-                            return (<HostDelete key={event.id} event={event} deleteEvent={this.props.deleteEvent}/>)})}    
-                        </div>
-                        </label>
-                    </div>
-                    
+            return <div className="dash-content">
+                <div className="dash-side">
+                  <h1 className="welcome">Welcome home!</h1>
+                  <p className="message">
+                    What are you grateful for today?
+                  </p>
+                  <div className="join-link">
+                    <Link to={"/events"}>
+                      Sign up to join a match!
+                    </Link>
+                  </div>
                 </div>
-            )
+
+                <div className="user-events">
+                    <label><h1>Events You JOINED...</h1></label>
+                  <div className="hosted-events">
+                    {this.props.joinedEvents.map(event => {
+                      return <UserLeave key={event.id} event={event} deleteJoin={this.props.deleteJoin} />;
+                    })}
+                  </div>
+
+                    <label> <h1>Events You HOSTED...</h1></label>
+                  <div className="hosted-events">
+                    {this.props.hostedEvents.map(event => {
+                      return <HostDelete key={event.id} event={event} deleteEvent={this.props.deleteEvent} />;
+                    })}
+                  </div>
+                </div>
+              </div>;
         }
     }
 }
