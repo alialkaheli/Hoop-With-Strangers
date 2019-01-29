0318bdcb043e85e5f4f87fb3ec281a89702891a8
ali alkaheli <ali.alkaheli96@gmail.com>
2019-01-29 15:14:35 -0800

    Fixed hosted part of the dashboard

--- frontend/components/dashboard/host_delete.jsx
+++ frontend/components/dashboard/host_delete.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 const HostDelete = (props) => {
+  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  let date = new Date(props.event.date).getUTCDay();
+  let weekday = weekdays[date];
   const obj = props.event.date.split("-");
   let yr = obj[0]
 
   let day = obj[2]
   const monthName = { "01": "January", "02": "Febuary", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "August", "09": "September", "10": "October", "11": "November", "12": "December" };
-  
+
   let month = monthName[obj[1].toString()]
   let ampm = false;
   let setTime = props.event.time.split(":")
@@ -21,26 +24,43 @@ const HostDelete = (props) => {
   } else {
     setTime += "AM"
   }
-  return(
-  <div>
-    <Link to={`/events/${props.event.id}`}>
-        <div className="event-attr">
-          <p className="event-time">
-          
-             {month} {day},
-        </p>
-          <p className="event-time">{yr}</p>
-          <p className="event-time">{setTime}</p>
-          <p className="event-address1">Location: </p>
-          <p className="event-address">{props.event.address}</p>
+  return <div className="dash-row">
+    <div className="dash-card">
+      <Link to={`/events/${props.event.id}`}>
+        <div className="top-dash-card">
+          <p className="dash-address1">{weekday}</p>
+          <p className="dash-date">
+            {month} {day}
+          </p>
+          <p className="dash-time">{setTime}</p>
+          <p className="dash-address">{props.event.address}</p>
         </div>
+      </Link>
 
-    </Link>
-      <div className="event-select1">
-        <div className="edit-btn"><Link  to={`/events/${props.event.id}/edit`}>Edit</Link></div>
-        <div className="edit-btn" onClick={() => props.deleteEvent(props.event.id)}>Delete</div>
+      <div className="bottom-dash-card">
+        <div className="dash-edit-btn" onClick={() => props.deleteJoin(props.event.id)}>
+          CANCEL MY SPOT
+          </div>
       </div>
     </div>
-);
+    <div className="dash-host">
+      <p>Get to know your host</p>
+      <div className="dash-sub-div">
+        <div className="profile-pic-events" />
+        <p>
+          Keep an eye open for your host! So it's easier, here's what they
+          look like :).
+          </p>
+      </div>
+      <div className="dash-buttons">
+        <button className="no-cursor-fb">
+          Facebook
+          </button>
+        <button className="no-cursor-twitter">
+          Twitter
+          </button>
+      </div>
+    </div>
+  </div>;
   }
 export default withRouter(HostDelete);
