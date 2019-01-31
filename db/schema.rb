0cd42c2b626df2ad80c552f7dbe4b90508de53cb
ali alkaheli <ali.alkaheli96@gmail.com>
2019-01-31 10:55:38 -0800

    Add spot column in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_180815) do
+ActiveRecord::Schema.define(version: 2019_01_31_183943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_12_18_180815) do
     t.string "city", null: false
     t.string "url"
     t.integer "players"
+    t.integer "spots", default: 9
   end
 
   create_table "join_tables", force: :cascade do |t|
